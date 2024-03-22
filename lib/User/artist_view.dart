d29f5d8d72ec7ad0805c8b412ce01b263da4606a
Deepak S <deepakthelakkad@gmail.com>
2024-03-22 17:45:58 +0530

    updated Design

--- lib/User/artist_view.dart
+++ lib/User/artist_view.dart
@@ -18,21 +18,48 @@ class _ArtistViewState extends State<ArtistView> {
           children: [
             Container(
               child: Row(
-
-              ),
-              height: 200,
-              decoration: BoxDecoration(image:DecorationImage(
-                  image: AssetImage('assets/A14.png'), // Replace with your image path
-                  fit: BoxFit.cover),borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
-                  gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromRGBO(60, 170, 212, 1),
-                  Color.fromRGBO(109, 102, 101, 1),
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ClipOval(
+                    child: Image.asset(
+                      "assets/AMLU.png",
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Amaleswar",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
 
                 ],
-              )),
+              ),
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                          'assets/A14.png'), // Replace with your image path
+                      fit: BoxFit.cover),
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20)),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromRGBO(60, 170, 212, 1),
+                      Color.fromRGBO(109, 102, 101, 1),
+                    ],
+                  )),
             ),
             Card(
               color: Colors.red.shade50,
@@ -60,11 +87,16 @@ class _ArtistViewState extends State<ArtistView> {
                       fontSize: 15, color: Color.fromRGBO(134, 135, 142, 1)),
                 ),
                 trailing: Wrap(children: [
-                  Icon(CupertinoIcons.star_fill,color: Color.fromRGBO(230, 210, 28, 1)),
-                  Icon(CupertinoIcons.star_fill,color: Color.fromRGBO(230, 210, 28, 1)),
-                  Icon(CupertinoIcons.star_fill,color: Color.fromRGBO(230, 210, 28, 1)),
-                  Icon(CupertinoIcons.star_fill,color: Color.fromRGBO(230, 210, 28, 1)),
-                  Icon(CupertinoIcons.star_fill,color: Color.fromRGBO(217, 217, 217, 1)),
+                  Icon(CupertinoIcons.star_fill,
+                      color: Color.fromRGBO(230, 210, 28, 1)),
+                  Icon(CupertinoIcons.star_fill,
+                      color: Color.fromRGBO(230, 210, 28, 1)),
+                  Icon(CupertinoIcons.star_fill,
+                      color: Color.fromRGBO(230, 210, 28, 1)),
+                  Icon(CupertinoIcons.star_fill,
+                      color: Color.fromRGBO(230, 210, 28, 1)),
+                  Icon(CupertinoIcons.star_fill,
+                      color: Color.fromRGBO(217, 217, 217, 1)),
                 ]),
               ),
             ),
